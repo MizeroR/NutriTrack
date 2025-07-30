fc72774ed25aa9765a5a825124cbb68e625fe80e
Hirwabrian <H.brian@alustudent.com>
2025-07-30 14:32:51 +0200

    made changes to the routing

--- nutritrack_test/lib/main.dart
+++ nutritrack_test/lib/main.dart
@@ -7,12 +7,13 @@ import 'screens/auth_wrapper.dart';
 import 'screens/signup_page.dart';
 import 'Navigation/nav_state.dart';
 import 'screens/patients_dashboard.dart';
-import 'screens/reports_dashboard.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:nutritrack_test/services/api_service.dart';
 import 'package:nutritrack_test/services/notification_service.dart';
-import '../navigation/persistent_layout.dart';
+import 'screens/reports_analytics_screen.dart';
+import 'screens/alert_management.dart';
+import '../widgets/options_menu_sheet.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,6 +46,15 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static void showOptionsMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (_) => const OptionsMenuSheet(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -58,12 +68,36 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         '/wrapper': (context) => const AuthWrapper(),
-        '/home': (context) => const PersistentLayout(child: MainScreen()),
+        '/home': (context) => const MainScreen(),
         '/signup': (context) => const SignUpScreen(),
-        '/patients_dashboard': (context) =>
-            const PersistentLayout(child: PatientsDashboard()),
-        '/reports_dashboard': (context) =>
-            const PersistentLayout(child: ReportsAnalyticsScreen()),
+        '/patients_dashboard': (context) => Scaffold(
+          body: const PatientsDashboard(),
+          floatingActionButton: FloatingActionButton(
+            heroTag: 'add_patient_fab',
+            backgroundColor: Colors.green.shade700,
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            onPressed: () => showOptionsMenu(context),
+            child: const Icon(Icons.add, color: Colors.white, size: 28),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+          extendBody: true,
+        ),
+        '/reports_analytics': (context) => Scaffold(
+          body: const ReportsAnalyticsScreen(),
+          floatingActionButton: FloatingActionButton(
+            heroTag: 'reports_analytics_fab',
+            backgroundColor: const Color(0xFF7BAC73),
+            onPressed: () {
+              // Action for the FAB
+            },
+            child: const Icon(Icons.add, size: 30),
+          ),
+          extendBody: true,
+        ),
+        '/alert_management': (context) => const AlertManagementScreen(),
       },
     );
   }
